0435ab8bb3655e399dfbae0c2d28d3336e292843
tandavala <tandavala@github.com>
2018-01-18 11:54:15 +0100

    adding decouple configuration and import token

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Ola mundo")
+import time
+import urllib3
+
+import telepot
+from decouple import config
+
+
+TOKEN = config('TOKEN')
+
+try:
+    print(TOKEN)
+    print("TOKEN is working...")
+except:    
+    print("Not working")
+
+
