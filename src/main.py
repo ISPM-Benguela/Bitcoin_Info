f1841b5085add203502388a212190f8f25967974
tandavala <tandavala@github.com>
2018-01-18 12:51:51 +0100

    python anyhere config

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import time
+
 import urllib3
 
 import telepot
 from decouple import config
 
+# Podes abandonar estas linhas de codigo caso nao estaras
+# Usando pythonanywhere free account
+
+proxy_url = 'http://proxy.server:3128'
+telepot.api._pools = {
+            'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+            }
+telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+# FIm para pythonanywhere acctount free
 
 TOKEN = config('TOKEN')
 bot = telepot.Bot(TOKEN)
