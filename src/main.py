8a474fca9f737b3594e3bbc4d3df603406d75b24
tandavala <tandavala@github.com>
2018-01-18 12:11:43 +0100

    initial commit

--- src/main.py
+++ src/main.py
@@ -6,11 +6,18 @@ from decouple import config
 
 
 TOKEN = config('TOKEN')
+bot = telepot.Bot(TOKEN)
 
-try:
-    print(TOKEN)
-    print("TOKEN is working...")
-except:    
-    print("Not working")
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
 
+    if content_type == 'text':
+        bot.sendMessage(chat_id, "You said '{}' ".format(msg['text']))
+
+bot.message_loop(handle)
+
+print('Listing...')
+while 1:
+    time.sleep(10)
 
