f04c7d6584cb2a97abe279ec792244ca40a10793
tandavala <tandavala@github.com>
2018-01-18 15:47:35 +0100

    adding eliza bot

--- src/main.py
+++ src/main.py
@@ -8,29 +8,31 @@ from nltk.chat.eliza import eliza_chatbot
 
 # Podes abandonar estas linhas de codigo caso nao estaras
 # Usando pythonanywhere free account
-
+"""
 proxy_url = 'http://proxy.server:3128'
 telepot.api._pools = {
             'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
             }
 telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 # FIm para pythonanywhere acctount free
-
+"""
 TOKEN = config('TOKEN')
 bot = telepot.Bot(TOKEN)
 
 def handle(msg):
+    """ processando o envio e de mensagens. """
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
     if content_type == 'text':
-        return
-    elif msg['text'].startswith('/ajuda'):
-        bot.setMessage(chat_id, 'Ola eu sou a filha da Eliza Bot vou te ensisar Igles Diz qualquer coisa em Ingles')
-    else:
-        bot.sendMessage(chat_id, eliza_chatbot.response(msg['text']))
-bot.message_loop(handle)
+        bot.sendMessage(chat_id, eliza_chatbot.respond(msg['text']))
 
+    if msg['text'].startswith('/ajuda'):
+            bot.sendMessage(chat_id, 'Ola eu sou KAMBA vamos falar English diz qualquer coisa EX: HOW ARE YOU?\nQuem me probramou chama -se TANDAVALA')
+
+    else:
+        bot.sendMessage(chat_id, eliza_chatbot.respond(msg['text']))
+bot.message_loop(handle)
 print('Listing...')
 while 1:
     time.sleep(10)
